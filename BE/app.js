ff287b037de6ddc308b2c4cf81e27c754c10206e
carlos <bobcn_1991@hotmail.com>
2018-06-20 00:38:37 +0100

    comments

--- BE/app.js
+++ BE/app.js
@@ -3,9 +3,11 @@ const app = express()
 const $ = require('jquery')
 
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: false, webPreferences: {webSecurity:false}})
+const nightmare = Nightmare({ show: true, webPreferences: {webSecurity:false}})
 
 //click on each add in order to see the date
+//see how many .search_result divs there are, and iterate over them,
+// but there is no index so it doesnt know who is next.
 app.get('/date',(req, res)=> {
   nightmare
   .goto('http://www.rent.ie/houses-to-let/renting_dublin/page_1/')
@@ -14,18 +16,14 @@ app.get('/date',(req, res)=> {
   .wait(2000)
   .evaluate(()=>{
     var search_results = []
-    
-      result = {}
-      result['price'] = $('#smi_main_box div.text h2').text().replace(/\s/g,'')
-      result['address'] = $('div.smi_main_top h1').text().replace(/\s/g,'')
-      result['date'] = document.querySelector('.description p').innerText.replace('Available from:', '')
-
-
-      search_results.push(result)
-      
-    
+    result = {}
+    result['price'] = $('#smi_main_box div.text h2').text().replace(/\s/g,'')
+    result['address'] = $('div.smi_main_top h1').text().replace(/\s/g,'')
+    result['date'] = document.querySelector('.description p').innerText.replace('Available from:', '')
+    search_results.push(result)
     return search_results
   })
+  .end()
   .then(console.log)
   .catch(error=>{
     console.error('error', error)
@@ -34,6 +32,7 @@ app.get('/date',(req, res)=> {
 })
 
 //all adds per page but not showing the date
+//get 20 per page, save, next page, same , next page, same...
 app.get('/nodate', (req, res) => {
 
   nightmare
@@ -50,13 +49,11 @@ app.get('/nodate', (req, res) => {
    })
    return search_results
   })
-
   .end()
   .then(console.log)
   .catch(error => {
     console.error('Search failed:', error)
   })
-
   res.send('Hello World!')
   }
 )
